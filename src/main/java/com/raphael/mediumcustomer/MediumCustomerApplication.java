abb217a04b7fbfc21e7ae4245a8abed0490f87a4
raphzam <60793109+raphzam@users.noreply.github.com>
2021-07-27 12:31:24 -0400

    Initial commit

---
+++ src/main/java/com/raphael/mediumcustomer/MediumCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.raphael.mediumcustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediumCustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediumCustomerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raphael/mediumcustomer/MediumCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphael.mediumcustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediumCustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
